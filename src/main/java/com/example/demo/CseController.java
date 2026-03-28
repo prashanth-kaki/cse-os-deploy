93ad96f485aa7a1da79dccaeb70430fe25efc977
Prashanth Kaki <prashanth49kaki@gmail.com>
2026-03-28 16:00:46 +0530

    Update CseController.java
    changed

--- src/main/java/com/example/demo/CseController.java
+++ src/main/java/com/example/demo/CseController.java
@@ -8,6 +8,13 @@ public class CseController {
 
     @GetMapping("/home")
     public String disp() {
-        return "WELCOME TO OPENSHIFT DEPLOYMENT";
+        int number = 5;
+        String result = "";
+
+        for(int i = 1; i <= 10; i++) {
+            result += number + " x " + i + " = " + (number * i) + "<br>";
+        }
+
+        return result;
     }
 }
